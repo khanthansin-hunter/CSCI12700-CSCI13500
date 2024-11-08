1f4ba297435464c127c792915b55f1becbbf8132
Leo <khantthan.sin06@myhunter.cuny.edu>
2024-11-07 21:39:31 -0500

    final version

--- snow_count.py
+++ snow_count.py
@@ -11,17 +11,12 @@ def counting_snow():
     plt.imshow(image_feed)
     plt.show()
     countSnow = 0
-    countSnow2 = 0
     white = 0.75
     for x in range (image_feed.shape[0]):       #for all width pixels given shape function
         for y in range(image_feed.shape[1]):    #for all height pixels given shape function
             if (image_feed[x,y,0]>white) and (image_feed[x,y,1]>white) and (image_feed[x,y,2]>white):
             #[width,height,red] and [width,height,green] and [width,height,blue]
                 countSnow = countSnow+1
-            if (image_feed[x,y,0]>white) or (image_feed[x,y,1]>white) or (image_feed[x,y,2]>white):
-            #[width,height,red] and [width,height,green] and [width,height,blue]
-                countSnow2 = countSnow2+1
     print('snow count of',countSnow)
-    print('snow count of',countSnow2)
-
+    
 counting_snow()
