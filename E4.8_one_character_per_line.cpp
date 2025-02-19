23228b8f62aec70525c042940f747209dd17ce95
Leo <khantthan.sin06@myhunter.cuny.edu>
2025-02-19 03:48:54 -0500

    Add files via upload

--- E4.8_one_character_per_line.cpp
+++ E4.8_one_character_per_line.cpp
@@ -1,11 +1,3 @@
-/*
-Name:       Khant Than Sin @Leo
-Email:      khantthan.sin06@myhunter.cuny.edu
-course:     csci13500
-instructor: Tong Yi
-
-This program takes input from user and prints the input in one character per line.
-*/
 #include <iostream>
 #include <string>
 using namespace std;
@@ -14,12 +6,12 @@ int main()
 {
     string input_characters;
     cout << "Enter a word: ";
-    getline(cin,input_characters);
+    getline(cin, input_characters);
     int index_count = 0;
     while (index_count < input_characters.length())
     {
         cout << input_characters[index_count] << endl;
-        index_count ++;
+        index_count++;
     }
     return 0;
 }
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# cs127-f24
-
-# this respository is intended to help cs127 students who are new to computer science with lab and assignments
+# cs135-s25
