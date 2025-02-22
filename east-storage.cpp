b7ad4ea38ceb5913a7a14321b5a4a6d1dbdfbf6a
Leo <khantthan.sin06@myhunter.cuny.edu>
2025-02-22 16:56:43 -0500

    lab1A first testing

--- east-storage.cpp
+++ east-storage.cpp
@@ -1,8 +1,37 @@
 #include <fstream>
 #include <cstdlib>
 #include <climits>
+#include <iostream>
+using namespace std;
 
 int main()
 {
-    
+    ifstream fin("Current_reservoir_Levels.tsv");
+    if (fin.fail())
+    {
+        cerr << "File cannot be opened" << endl;
+        exit(1);
+    }
+
+    string headerline;
+    getline(fin,headerline);
+    string date;
+    double eastSt;
+    double eastEl;
+    double westSt;
+    double westEl;
+
+    while (fin >> date >> eastSt >> eastEl >> westSt >> westEl)
+    {
+        fin.ignore(INT_MAX, '\n');
+        string user_input;
+        cout << "Enter a date in (MM/DD/YYYY) format" << endl;
+        getline(cin, user_input);
+        cout << user_input;
+        if (user_input == date)
+        {
+            cout << user_input << " " << eastSt;
+        }
+    }
+    fin.close();
 }
